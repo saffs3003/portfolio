41a1c4ffdab05b32fe15400246fcb27c51b32e67
Saffi <maharjansaffi@gmail.com>
2024-10-02 00:24:59 +0545

    Initial commit

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,68 @@
+import styles from './HeroStyles.module.css';
+import heroImg from '../../assets/profile.jpg';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import twitterLight from '../../assets/twitter-light.svg';
+import twitterDark from '../../assets/twitter-dark.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import CV from '../../assets/cv.pdf';
+import { useTheme } from '../../common/ThemeContext';
+
+function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === 'light' ? sun : moon;
+  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+
+  return (
+    <section id="hero" className={styles.container}>
+      <div className={styles.colorModeContainer}>
+        <img
+          src={heroImg}
+          className={styles.hero}
+          alt="Profile picture of Saffi Maharjan"
+          style={{ borderRadius: '50%', width: '300px', height: '300px', objectFit: 'cover' }}
+        />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color mode icon"
+          onClick={toggleTheme}
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Saffi
+          <br />
+          Maharjan
+        </h1>
+        <h2>UI/UX Designer</h2>
+        <span>
+          <a href="https://twitter.com/" target="_blank">
+            <img src={twitterIcon} alt="Twitter icon" />
+          </a>
+          <a href="https://github.com/saffi-saffs" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
+          </a>
+          <a href="https://np.linkedin.com/in/saffimaharjan" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin icon" />
+          </a>
+        </span>
+        <p className={styles.description}>
+        Passionate about creating user-centered designs and intuitive interfaces
+        ensuring seamless user experiences across digital products
+        </p>
+        <a href={CV} download>
+          <button className="hover">Resume</button>
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,51 @@
+import styles from './ProjectsStyles.module.css';
+import textcolab from '../../assets/9.png';
+import ui from '../../assets/ui.jpeg';
+
+import Ecommerce from '../../assets/3.png';
+import DigiLocker from '../../assets/digiloc.png';
+import FakeNews from '../../assets/news.jpg';
+import ProjectCard from '../../common/ProjectCard';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+      <ProjectCard
+          src={ui}
+          link="https://github.com/saffi-saffs/CollabTextEditor"
+          h3="UI/UX projects"
+          p="UI/UX designs"
+        />
+      <ProjectCard
+          src={textcolab}
+          link="https://github.com/saffi-saffs/CollabTextEditor"
+          h3="Collab Text"
+          p="Collaborative Text Editor"
+        />
+       
+        <ProjectCard
+          src={Ecommerce}
+          link="https://github.com/saffi-saffs/E-commerceApp"
+          h3="E-Commerce App"
+          p="Clothing app"
+        />
+        <ProjectCard
+          src={DigiLocker}
+          link="https://github.com/saffi-saffs/DigiLocker"
+          h3="DigiLoc"
+          p="Digital Document locker"
+        />
+        <ProjectCard
+          src={FakeNews}
+          link="https://github.com/saffi-saffs/FakeNews"
+          h3="News Portal"
+          p="News App & fake News Detector"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
