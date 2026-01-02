d324f1f807bbaeca1d96cbdb417d5a708e45ae75
user.email <maharjansaffi@gmail.com>
2026-01-02 10:15:38 +0545

    v2 email fixes

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -16,7 +16,7 @@ function Hero(props) {
           I'm a software engineer and artist based in Kathmandu. I'm fascinated
           by large-scale, high-impact products.
         </p>
-        <a href="mailto:maharjansaffi@gmail.com">
+        <a href="mailto:connect@saffimaharjan@gmail.com">
           <Button
             variant="outlined"
             size="large"
